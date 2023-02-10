d269d9bbae5cc728206a0c1c3f2f1f708db9a0d2
Mykola Dovhopol <97382411+lifesheets@users.noreply.github.com>
2023-02-10 18:31:37 +0200

    [Fix] Перевірка "У Вас немає причепа".

--- packages/managers/trucker.js
+++ packages/managers/trucker.js
@@ -1663,18 +1663,19 @@ trucker.doneOffer = function (player, offerId) {
         catch (e) {
             
         }
-    }
-    else {
+    } else {
 
-        let trailerId = player.vehicle.getVariable('trailer');
+        let trailer = null;
+        mp.vehicles.forEach(function (v) {
+            if (v.getVariable('trOwner') == user.getId(player)) trailer = v;
+        });
 
-        if (!trailerId) {
+        if(trailer == null)
+        {
             trucker.notify(player, '~r~У Вас нет прицепа');
             trucker.removeOffer(offerId);
             return;
         }
-
-        let trailer = mp.vehicles.at(player.vehicle.getVariable('trailer'));
 
         if (!vehicles.exists(trailer)) {
             trucker.removeOffer(offerId);
