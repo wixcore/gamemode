a86c2bfb42df216380b5d5a4bed90a7e805538aa
Mykola Dovhopol <97382411+lifesheets@users.noreply.github.com>
2023-02-10 18:21:36 +0200

    [Format] Patch :: handlers\src\manager\discord.js

--- handlers/src/manager/discord.js
+++ handlers/src/manager/discord.js
@@ -1,40 +1,43 @@
+import methods from '../modules/methods';
 import user from '../user';
 
 let discord = {};
 
-discord.checker = function() {
-    let label = 'STATE 99 ROLE PLAY';
+discord.checker = function () {
+    let label = 'Скачать бесплатно State-99';
     try {
         if (user.isLogin()) {
-            if (mp.players.local.isDiving())
+            if (mp.players.local.isDiving()) {
                 label = 'Занимается дайвингом';
-            else if (mp.players.local.isSwimming() || mp.players.local.isSwimmingUnderWater())
+            } else if (mp.players.local.isSwimming() || mp.players.local.isSwimmingUnderWater()) {
                 label = 'Плавает';
-            else if (mp.players.local.isFalling())
+            } else if (mp.players.local.isFalling()) {
                 label = 'Падает с высоты';
-            else if (mp.players.local.isRagdoll())
+            } else if (mp.players.local.isRagdoll()) {
                 label = 'Лежит на земле';
-            else if (mp.players.local.isDead())
+            } else if (mp.players.local.isDead()) {
                 label = 'Отдыхает в коме';
-            else if (mp.players.local.isInAnyVehicle(false))
+            } else if (mp.players.local.isInAnyVehicle(false)) {
                 label = 'В транспорте';
-            else if (mp.players.local.isRunning() || mp.players.local.isSprinting())
+            } else if (mp.players.local.isRunning() || mp.players.local.isSprinting()) {
                 label = 'Бегает';
-            else if (mp.players.local.isShooting())
+            } else if (mp.players.local.isShooting()) {
                 label = 'Стреляет';
-            else if (mp.players.local.isWalking())
+            } else if (mp.players.local.isWalking()) {
                 label = 'Бродит';
-            else if (mp.players.local.getVariable('isAfk') === true)
+            } else if (mp.players.local.getVariable('isAfk') === true) {
                 label = 'Чиллит';
-            else
-                label = 'STATE 99 ROLE PLAY';
-        }
-        else
+            } else {
+                label = 'Скачать бесплатно State-99';
+            }
+        } else {
             label = 'В меню авторизации';
+        }
     }
     catch (e) {
+        methods.debug('Discord Widget', e)
     }
-    mp.discord.update(label, 'state-99.com');
+    mp.discord.update(label, 'wixcore.net');
 };
 
 export default discord;
